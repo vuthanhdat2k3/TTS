29c599803ebbf1098426939bde9225dd55134377
vuthanhdat2k3 <vuthanhdat19052k3@gmail.com>
2024-07-30 18:24:15 +0700

    first commit

--- TTS/demos/xtts_ft_demo/utils/formatter.py
+++ TTS/demos/xtts_ft_demo/utils/formatter.py
@@ -53,7 +53,7 @@ def format_audio_list(audio_files, target_language="en", out_path=None, buffer=0
     device = "cuda" if torch.cuda.is_available() else "cpu" 
 
     print("Loading Whisper Model!")
-    asr_model = WhisperModel("large-v2", device=device, compute_type="float16")
+    asr_model = WhisperModel("large-v2", device=device, compute_type="float32")
 
     metadata = {"audio_file": [], "text": [], "speaker_name": []}
 
